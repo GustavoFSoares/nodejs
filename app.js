79d247e4971493ee76c2df3f86053b96938f1fda
Gustavo Soares <gustavo10.fsoares@gmail.com>
2018-01-16 19:45:14 -0200

    ultimo commit para projeto padrao

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-var express = require("express");
-var app = express();
+var app = require('./config/express')();
+var rotasDosProdutos = require('./app/routes/produtos')(app);
 
-app.set('view engine', 'ejs');
-
-app.get("/produtos", function(req, res){
-    res.render("produtos/lista");
-});
-
-app.listen(3000, function(){
-    console.log("Deu boa");
-});
+app.listen(3000, function(){});
